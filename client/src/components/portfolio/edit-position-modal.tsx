e3a9a36474d407d8928774a4bdd63bb11fef7250
saifuluvs-eng <saifuluvs@gmail.com>
2025-09-19 18:16:03 +0530

    Update edit-position-modal.tsx

--- client/src/components/portfolio/edit-position-modal.tsx
+++ client/src/components/portfolio/edit-position-modal.tsx
@@ -1,4 +1,5 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useEffect } from "react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
