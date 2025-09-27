afdb818fc5e93490bc17ecc6fabb73a93b41b475
saifuluvs-eng <saifuluvs@gmail.com>
2025-09-28 05:29:36 +0530

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,14 @@
-import './lib/disableLocalWs';
+import "./lib/disableLocalWs";
+import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+// ⬇️ wrap App with the provider that your hook expects
+import { FirebaseAuthProvider } from "./hooks/useFirebaseAuth";
+
+createRoot(document.getElementById("root")!).render(
+  <FirebaseAuthProvider>
+    <App />
+  </FirebaseAuthProvider>
+);
