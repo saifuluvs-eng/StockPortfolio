9ef1fe68f934780bd1ad9054a82dba1dc41a710f
saifuluvs-eng <saifuluvs@gmail.com>
2025-09-28 05:37:57 +0530

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,11 +4,16 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-// ⬇️ wrap App with the provider that your hook expects
+// Firebase auth provider
 import { FirebaseAuthProvider } from "./hooks/useFirebaseAuth";
 
+// ⬇️ add HashRouter (prevents server 404s on deep links)
+import { HashRouter } from "react-router-dom";
+
 createRoot(document.getElementById("root")!).render(
-  <FirebaseAuthProvider>
-    <App />
-  </FirebaseAuthProvider>
+  <HashRouter>
+    <FirebaseAuthProvider>
+      <App />
+    </FirebaseAuthProvider>
+  </HashRouter>
 );
