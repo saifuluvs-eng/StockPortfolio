3bb96c5d40ef981321bd881a9d47252e73d42e85
saifuluvs-eng <saifuluvs@gmail.com>
2025-09-29 03:51:19 +0530

    Update ping.ts

--- api/ping.ts
+++ api/ping.ts
@@ -1,6 +1,4 @@
 // api/ping.ts
-import type { VercelRequest, VercelResponse } from "@vercel/node";
-
-export default function handler(_req: VercelRequest, res: VercelResponse) {
+export default function handler(_req: any, res: any) {
   res.status(200).json({ ok: true, message: "pong", time: new Date().toISOString() });
 }
