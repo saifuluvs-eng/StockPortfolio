d8ecbc850f6bdec2786996bd5f12a3631239c7ce
saifuluvs-eng <saifuluvs@gmail.com>
2025-09-30 20:24:01 +0530

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,12 +2,14 @@
 import "./lib/disableLocalWs";
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App";
 import "./index.css";
+import "./styles/tokens.css";
 
 import { FirebaseAuthProvider } from "./hooks/useFirebaseAuth";
 import { Router } from "wouter";
 import { useHashLocation } from "wouter/use-hash-location";
+
+import App from "./App";
 
 createRoot(document.getElementById("root")!).render(
   <Router hook={useHashLocation}>
