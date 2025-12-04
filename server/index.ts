ecb8577401b0caa7af96c9b6a4365f546ae1e91d
saifuluvs-eng <saifuluvs@gmail.com>
2025-12-04 19:07:36 +0530

    feat: add dotenv configuration for environment variables

--- server/index.ts
+++ server/index.ts
@@ -1,3 +1,4 @@
+import 'dotenv/config';
 import express, { type Request, type Response, type NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
