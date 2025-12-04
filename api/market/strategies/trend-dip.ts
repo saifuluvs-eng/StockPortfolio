dba96ce8de1ff6d4ae1490628ace4921ac4eba15
saifuluvs-eng <saifuluvs@gmail.com>
2025-12-04 20:56:49 +0530

    Fix: Move services to api/_lib to ensure Vercel bundling

--- api/market/strategies/trend-dip.ts
+++ api/market/strategies/trend-dip.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
-import { technicalIndicators } from "../../../server/services/technicalIndicators";
+import { technicalIndicators } from "../../_lib/technicalIndicators";
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
     try {
