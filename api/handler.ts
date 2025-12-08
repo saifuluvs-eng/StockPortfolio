fdbfa096130bbb057185510b47d90817d6db738b
saifuluvs-eng <saifuluvs@gmail.com>
2025-12-08 21:02:49 +0530

    fix: await server initialization in vercel handler to prevent race condition

--- api/handler.ts
+++ api/handler.ts
@@ -1,6 +1,7 @@
-import app from '../server/index';
+import app, { ready } from '../server/index';
 
 export default async function handler(req: any, res: any) {
+  await ready;
   // Pass the request to the Express app
   return app(req, res);
 }
--- server/index.ts
+++ server/index.ts
@@ -82,3 +82,7 @@ app.use((req, res, next) => {
 })();
 
 export default app;
+// Export a ready promise for Vercel handler
+export const ready = (async () => {
+    return registerRoutes(app);
+})();
